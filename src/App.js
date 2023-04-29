3f8e22f546108456b1c3d09655e33d9f1853f7e9
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-04-29 23:26:31 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
+import React from 'react';
 import './App.css';
+import data from './data.json';
 
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href='/'>React Shopping Cart</a>
-      </header>
-      <main>Product List</main>
-      <footer>All rights is required.</footer>
-    </div>
-  );
+class App extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: "", 
+    };
+  }
+  render() {
+    return (
+      <div className="grid-container">
+        <header>
+          <a href='/'>React Shopping Cart</a>
+        </header>
+        <main>
+          <div className='content'>
+            <div className='main'>Products</div>
+            <div className='sidebar'>Cart Items</div>
+          </div>
+        </main>
+        <footer>All rights is required.</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
