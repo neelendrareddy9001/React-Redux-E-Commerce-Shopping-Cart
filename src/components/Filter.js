a6dfe3bb9760ec23b0eb0c8e6ad10f667c10dc4c
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-05-01 21:44:23 +0200

    Update Filter.js

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -3,7 +3,29 @@ import React, { Component } from 'react'
 export default class Filter extends Component {
   render() {
     return (
-      <div>Filter</div>
+      <div className='filter'>
+        <div className='filter-result'>{this.props.count} Products</div>
+        <div className='filter-sort'>
+          Order
+          <select value={this.porps.size} onChange={this.props.sortProducts}>
+            <option>Latest</option>
+            <option value="lowest">Lowest</option>
+            <option value="highest">Highest</option>
+          </select>
+        </div>
+        <div className='filter-size'>
+          Filter
+          <select>
+            <option value="">All</option>
+            <option value="Xs">XS</option>
+            <option value="S">S</option>
+            <option value="M">M</option>
+            <option value="L">L</option>
+            <option value="XL">XL</option>
+            <option value="XXl">XXL</option>
+          </select>
+        </div>
+      </div>
     )
   }
 }
