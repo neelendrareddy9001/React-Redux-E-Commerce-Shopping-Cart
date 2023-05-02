3dbe8b1346caf0a8afb5f54174a35b9e427608d6
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-05-02 20:44:18 +0200

    Update Cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import formatCurrency from '../Utl';
 
 export default class Cart extends Component {
   render() {
@@ -12,6 +13,29 @@ export default class Cart extends Component {
                 You have {cartItems.length} in the cart{" "}
             </dv>
         )}
+        <div className='cart'>
+            <ul className='cart-items'>
+                {cartItems.map(item => (
+                    <li key={item._id}>
+                        <div>
+                            <img src={item.image} alt={item.title}></img>
+                        </div>
+                        <div>
+                        <div>{item.title}</div>
+                        <div className='right'>
+                            {formatCurrency(item.price)} x {item.count}{" "}
+                            <button 
+                                className='button'
+                                onClick={() => this.props.removeFromCart(item)}
+                            >Remove</button>
+                        </div>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+      </div>
+      </div>
       </div>
     )
   }
