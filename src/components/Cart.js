04fcc5691744f37eb27dc427aced6b78e97fb10e
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-05-02 19:38:17 +0200

    Update Cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,8 +2,17 @@ import React, { Component } from 'react'
 
 export default class Cart extends Component {
   render() {
+    const {cartItems} = this.props;
     return (
-      <div>Cart</div>
+      <div>
+        {cartItems.length === 0?(
+            <div className='cart cart-header'>Cart is empty</div>
+        ) : (
+            <dv className="cart cart-header">
+                You have {cartItems.length} in the cart{" "}
+            </dv>
+        )}
+      </div>
     )
   }
 }
