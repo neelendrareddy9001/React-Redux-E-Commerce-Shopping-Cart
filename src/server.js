17135f01dfd662267130ad3d70a45dfeaeba35ce
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-05-03 23:29:39 +0200

    Add files via upload

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const express = require ("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const shortid = require("shortid");
+
+const app = express();
+app.use(bodyParser);
+
+mongoose.connect("mongodb://localhost/react-shopping-cart-db", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+});
+
+app.get("/api/products", (req, res) => {
+    
+})
