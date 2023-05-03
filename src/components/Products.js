efc49b181e4bc189223337af22088e09e159779b
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-05-03 21:00:22 +0200

    Update Products.js

--- src/components/Products.js
+++ src/components/Products.js
@@ -38,6 +38,40 @@ export default class Products extends Component {
             ))}
         </ul>
        </Fade>
+       {product && (
+          <Modal isOpen={true} onRequestClose={this.closeModal}>
+            <Zoom>
+              <button className='close-modal' onClick={this.closeModa}>
+              x
+              </button>
+              <div className='product-details'>
+                <img src={product.image} alt={product.title}></img>
+                <div className='product-details-description'>
+                  <p>
+                    <strong>{product.title}</strong>
+                  </p>
+                  <p>{product.description}</p>
+                  <p>
+                    Available Sizes: {" "}
+                    {product.availableSizes.map((x) => (
+                      <span>
+                        {" "}
+                        <button className='button'>{x}</button>
+                      </span>
+                    ))}
+                  </p>
+                  <div className='product-price'>
+                    <div>{formatCurrency(product.price)}</div>
+                    <button className='button primary' onClick={() => {
+                      this.closeModal();
+                      this.props.addToCart(product);
+                    }}>Add To Cart</button>
+                  </div>
+                </div>
+              </div>
+            </Zoom>
+          </Modal>
+        )}
       </div>
     )
   }
